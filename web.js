dbc7f5b6d3ff6d80dd89d6104f2935f636793267
Coursera LP <courseralp@gmail.com>
2013-07-07 21:06:11 -0400

    Updated web.js to use index.html and updated index.html contents

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+// Assignment hints at using fs.readFileSync and buffers.
+// Use 'utf8' encoding to treat as string to make things simpler.
+
+var text = fs.readFileSync('index.html','utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
